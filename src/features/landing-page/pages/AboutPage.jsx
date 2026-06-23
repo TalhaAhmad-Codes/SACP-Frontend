d8a57d6896871e4e11aa85192073b9bde71ab086
Raza2911-rd <hafizraza2911@gmail.com>
2026-06-23 17:13:40 +0500

    changes done

--- src/features/landing-page/pages/AboutPage.jsx
+++ src/features/landing-page/pages/AboutPage.jsx
@@ -1,9 +1,9 @@
-import Navbar from "../../shared/components/Navbar"
+import Navbar from "../../../shared/components/Navbar"
 import FooterSection from "../about/components/FooterSection"
 import HeroSection from "../about/components/HeroSection"
 import PlatfornSection from "../about/components/PlatformSection"
 import TeamSection from "../about/components/Team"
-import { FadeUp } from "../../shared/components/FadeUp"
+import { FadeUp } from "../../../shared/components/FadeUp"
 
 
 function AboutPage(){
--- src/features/landing-page/pages/ContactPage.jsx
+++ src/features/landing-page/pages/ContactPage.jsx
@@ -1,5 +1,5 @@
-import Navbar from "../../shared/components/Navbar"
-import { FadeUp } from "../../shared/components/FadeUp"
+import Navbar from "../../../shared/components/Navbar"
+import { FadeUp } from "../../../shared/components/FadeUp"
 
 import ContactSection from "../contact/components/ContactSection"
 import ContactFooter from "../contact/components/Footer"
